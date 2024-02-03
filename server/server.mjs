51587cbcc0698aed1a58c0d06bdc8296b55e41dc
ryan <ryanlimxyy@gmail.com>
2024-02-03 18:23:56 +0800

    Increased payload limit for bigger image file sizes

--- server/server.mjs
+++ server/server.mjs
@@ -1,28 +1,35 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.mjs"
-import users from "./routes/user.mjs"
-import applications from "./routes/applications.mjs"
-import login from "./routes/login.mjs"
+import records from "./routes/record.mjs";
+import users from "./routes/user.mjs";
+import applications from "./routes/applications.mjs";
+import login from "./routes/login.mjs";
 
-
-const PORT = process.env.PORT || 5050;
+// Starting from Express 4.16.0, body-parser has been re-added under the methods express.json() and express.urlencoded()
 const app = express();
+const PORT = process.env.PORT || 5050;
 
-app.use(cors())
-app.use(express.json());
+app.use(cors());
 
-app.use("/record",records)
-app.use("/user",users)
-app.use("/application",applications)
-app.use("/login",login)
+// Increase limit for JSON payload
+app.use(express.json({ limit: '50mb' }));
 
+// Increase limit for URL-encoded payload
+app.use(express.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
 
-app.get("/",async (req, res)=>{
-    res.send("Hello World").status(200);
-})
+app.use("/record", records);
+app.use("/user", users);
+app.use("/application", applications);
+app.use("/login", login);
 
-//start the server
-app.listen(PORT,() =>{
-    console.log(`Server is running on port :http://localhost:${PORT}`);
-})
+app.get("/", async (req, res) => {
+    res.status(200).send("Hello World");
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: http://localhost:${PORT}`);
+});
